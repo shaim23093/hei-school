fcf402c771323fbd445c7fcb016beedd033fdee1
anthonyRanivoarison <antonitsiory@gmail.com>
2026-08-16 16:40:56 +0300

    style: format AuthResponse

--- src/main/java/school/hei/admin/dto/response/AuthResponse.java
+++ src/main/java/school/hei/admin/dto/response/AuthResponse.java
@@ -5,5 +5,4 @@ import lombok.Builder;
 import school.hei.admin.entity.enums.Role;
 
 @Builder
-public record AuthResponse(
-    String token, UUID accountId, String username, Role role) {}
+public record AuthResponse(String token, UUID accountId, String username, Role role) {}
