a7ca86bac983c63208b3a740281918adfad810e5
anthonyRanivoarison <antonitsiory@gmail.com>
2026-08-16 16:35:34 +0300

    refactor: format and resolve import in AuthResponse

--- src/main/java/school/hei/admin/dto/response/AuthResponse.java
+++ src/main/java/school/hei/admin/dto/response/AuthResponse.java
@@ -2,6 +2,8 @@ package school.hei.admin.dto.response;
 
 import java.util.UUID;
 import lombok.Builder;
+import school.hei.admin.entity.enums.Role;
 
 @Builder
-public record AuthResponse(String token, UUID accountId, String username, school.hei.admin.enums.Role role) {}
+public record AuthResponse(
+    String token, UUID accountId, String username, Role role) {}
