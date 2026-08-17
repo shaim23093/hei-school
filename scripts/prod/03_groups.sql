d90517ab5c47118fee34bee74ef10727a3ee00a0
anthonyRanivoarison <antonitsiory@gmail.com>
2026-08-17 20:47:59 +0300

    chore: add local scripts for database seed

---
+++ scripts/prod/03_groups.sql
@@ -0,0 +1,2 @@
+INSERT INTO "group" (id, name, path) VALUES ('g0000000-0000-0000-0000-000000000001', 'Group1', 'EL');
+INSERT INTO "group" (id, name, path) VALUES ('g0000000-0000-0000-0000-000000000002', 'Group2', 'TN');
